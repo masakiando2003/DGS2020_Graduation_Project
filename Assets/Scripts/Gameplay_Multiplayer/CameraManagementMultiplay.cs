1ae598c54da298209779fc63e3b0bc24b7f7cff9
Raymond Yeung <masakiando2003@gmail.com>
2021-12-16 16:44:55 +0900

    Gameplay Solo Multiplay 2021121601
    1. Change stages for multi-play
    2. Added to change background in options
    3. Change appearance for item box
    4. Fix bugs

--- Assets/Scripts/Gameplay_Multiplayer/CameraManagementMultiplay.cs
+++ Assets/Scripts/Gameplay_Multiplayer/CameraManagementMultiplay.cs
@@ -5,12 +5,20 @@ using UnityEngine.UI;
 
 public class CameraManagementMultiplay : MonoBehaviour
 {
+    [SerializeField] Transform defaultStageViewCameraPos;
+    [SerializeField] Text StageViewCameraControlsEN, StageViewCameraControlsJP;
+    [SerializeField] float stageViewCameraMoveFactor = 50f, stageViewCameraZoomFactor = 10f;
+    [SerializeField] float stageViewCameraMinFOV = 10f;
+    [SerializeField] float stageViewCameraHorizontalBoundary = 20f, stageViewCameraVerticalBoundary = 20f;
     [SerializeField] Camera playerCamera, stageViewCamera;
-    [SerializeField] GameObject boostSlider;
+    [SerializeField] GameObject boostSlider, playerItemLabels;
+    [SerializeField] Canvas playerRocketCanvas, stageViewCanvas;
     [SerializeField] GameObject playerRocketCurrentPositionArrowObject;
 
     int playerID;
     bool stageViewFlag;
+    Transform defaultStageViewCameraPosition;
+    float defaultStageViewCameraFoV, stageCameraMaxFov;
 
     private void Awake()
     {
@@ -33,11 +41,15 @@ public class CameraManagementMultiplay : MonoBehaviour
         {
             playerRocketCurrentPositionArrowObject.SetActive(false);
         }
+        defaultStageViewCameraPosition = defaultStageViewCameraPos;
+        defaultStageViewCameraFoV = stageViewCamera.fieldOfView;
+        stageCameraMaxFov = stageViewCamera.fieldOfView;
     }
 
     private void Update()
     {
         ResponseToChangeCamera();
+        ResponseToMoveStageViewCamera();
     }
 
     private void ResponseToChangeCamera()
@@ -47,24 +59,67 @@ public class CameraManagementMultiplay : MonoBehaviour
         {
             return;
         }
-        if (Input.GetButton(playerID + "PChangeCamera"))
+        if (Input.GetButtonDown(playerID + "PChangeCamera"))
         {
-            stageViewFlag = true;
+            stageViewFlag = !stageViewFlag;
+            SwitchCamera();
         }
-        else
-        {
-            stageViewFlag = false;
-        }
+    }
+
+    private void SwitchCamera()
+    {
         if (stageViewFlag)
         {
             ChangeToStageViewCamera();
             ActiveCurrentPositionArrowObject();
+            GetComponent<MovementMultiplay>().DisablePlayerControl();
         }
         else
         {
+            ResetStageViewCamera();
             ChangeToPlayerCamera();
             DeactivateCurrentPositionArrowObject();
+            GetComponent<MovementMultiplay>().EnablePlayerControl();
         }
+    }
+
+    private void ResponseToMoveStageViewCamera()
+    {
+        if (!stageViewFlag) { return; }
+        playerID = GetComponent<PlayerStatusMultiplay>().GetPlayerID();
+        float horizontal = Input.GetAxis(playerID+"PHorizontal");
+        float vertical = Input.GetAxis(playerID+"PVertical");
+        /*
+        if(stageViewCamera.transform.position.x >= defaultStageViewCameraPosition.position.x - stageViewCameraHorizontalBoundary && 
+           stageViewCamera.transform.position.x <= defaultStageViewCameraPosition.position.x + stageViewCameraHorizontalBoundary &&
+           stageViewCamera.transform.position.y >= defaultStageViewCameraPosition.position.y - stageViewCameraVerticalBoundary &&
+           stageViewCamera.transform.position.y <= defaultStageViewCameraPosition.position.y + stageViewCameraVerticalBoundary)
+        {
+            stageViewCamera.transform.Translate(horizontal * stageViewCameraMoveFactor * Time.deltaTime, vertical * stageViewCameraMoveFactor * Time.deltaTime, 0f);
+        }
+        */
+        stageViewCamera.transform.Translate(horizontal * stageViewCameraMoveFactor * Time.deltaTime, vertical * stageViewCameraMoveFactor * Time.deltaTime, 0f);
+
+        if (Input.GetButton(playerID + "PZoomIn"))
+        {
+            stageViewCamera.fieldOfView -= stageViewCameraZoomFactor * Time.deltaTime;
+            stageViewCamera.fieldOfView = Mathf.Clamp(stageViewCamera.fieldOfView, stageViewCameraMinFOV, stageCameraMaxFov);
+        }
+        else if (Input.GetButton(playerID + "PZoomOut"))
+        {
+            stageViewCamera.fieldOfView += stageViewCameraZoomFactor * Time.deltaTime;
+            stageViewCamera.fieldOfView = Mathf.Clamp(stageViewCamera.fieldOfView, stageViewCameraMinFOV, stageCameraMaxFov);
+        }
+        else if (Input.GetButtonDown(playerID + "PZoomReset"))
+        {
+            ResetStageViewCamera();
+        }
+    }
+
+    private void ResetStageViewCamera()
+    {
+        stageViewCamera.transform.position = defaultStageViewCameraPosition.position;
+        stageViewCamera.fieldOfView = defaultStageViewCameraFoV;
     }
 
     private void ActiveCurrentPositionArrowObject()
@@ -83,6 +138,9 @@ public class CameraManagementMultiplay : MonoBehaviour
         playerCamera.enabled = true;
         stageViewCamera.enabled = false;
         boostSlider.SetActive(true);
+        if (playerItemLabels== null || stageViewCanvas == null) { return; }
+        playerItemLabels.SetActive(true);
+        stageViewCanvas.enabled = false;
     }
 
     public void ChangeToStageViewCamera()
@@ -91,5 +149,25 @@ public class CameraManagementMultiplay : MonoBehaviour
         stageViewCamera.enabled = true;
         playerCamera.enabled = false;
         boostSlider.SetActive(false);
+        if (playerItemLabels == null || stageViewCanvas == null) { return; }
+        playerItemLabels.SetActive(false);
+        stageViewCanvas.enabled = true;
+
+        if (Language.gameDisplayLanguage == Language.DisplayLanauge.None)
+        {
+            Language.gameDisplayLanguage = Language.DisplayLanauge.English;
+        }
+        switch (Language.gameDisplayLanguage)
+        {
+            case Language.DisplayLanauge.English:
+                StageViewCameraControlsEN.enabled = true;
+                StageViewCameraControlsJP.enabled = false;
+                break;
+            case Language.DisplayLanauge.Japanese:
+                StageViewCameraControlsEN.enabled = false;
+                StageViewCameraControlsJP.enabled = true;
+                break;
+
+        }
     }
 }
