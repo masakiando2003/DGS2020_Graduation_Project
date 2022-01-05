93de29edcd018faf9edf885a971b03a38cf6efa3
Raymond Yeung <masakiando2003@gmail.com>
2022-01-06 01:07:31 +0900

    Online Gameplay 20220106
    Added Photon Module

---
+++ Assets/Scripts/Gameplay_Online/Gamplay_Online.cs
@@ -0,0 +1,21 @@
+using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gamplay_Online : MonoBehaviourPunCallbacks
+{
+    Transform[] playerPositions;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
